b86dba4eac300461123bb7cafd6f2461c4500066
mikelbrierly <mikel@hey.com>
2021-03-01 11:43:24 -0600

    added copyable code

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const forkBannerUrls = ["https://res.cloudinary.com/mikel-howarth/image/upload/v
 const main = document.querySelector('main');
 
 function bannerMaker(url) {
+  const anchor = `<a href="https://github.com/link-to-your-repo" target="_blank"><img src="${url}" alt="Fork me on GitHub!" style="position: absolute;right: 0;top: 0;height: 100px;"></a>`;
+  //TODO: make dynamic with REGEX or string replacement
+  const anchorEncoded = `&lt;a href=&quot;https://github.com/link-to-your-repo&quot; target=&quot;_blank&quot;>&lt;img src=&quot;${url}&quot; alt=&quot;Fork me on GitHub!&quot; style=&quot;position: absolute;right: 0;top: 0;height: 100px;&quot;&gt;&lt;/a&gt;`;
+
   const forkBanner = `<div class="fork-banner-container">
-                        <a href="https://github.com/link-to-your-repo" target="_blank"><img src="${url}" alt="Fork me on GitHub!" style="position: absolute;right: 0;top: 0;height: 100px;"></a>
+                        ${anchor}
+                        <pre><code>${anchorEncoded}</code></pre>
                       </div>`
   return forkBanner;
 }
